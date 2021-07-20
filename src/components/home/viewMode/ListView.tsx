8a1e93edaeb7af67318be704c1d5ae621a011dea
Phạm Duẩn <mophm99@gmail.com>
2021-07-20 16:32:12 +0700

    undo redo

--- src/components/home/viewMode/ListView.tsx
+++ src/components/home/viewMode/ListView.tsx
@@ -10,10 +10,10 @@ const ListView: React.FunctionComponent<IListViewProps> = (props) => {
   const img: Array<object> = useSelector((state: any) => state.img)
   return (
     <Row>
-      {img.map((ele: any) => {
+      {img.map((ele: any, index) => {
         const url = `https://picsum.photos/id/${ele.id}/400`
         return (
-          <Card >
+          <Card key={index}>
             <Card.Body className='d-flex justify-content-between align-items-center'>
               <Card.Title className='text-capitalize text-center'>{ele.title}</Card.Title>
               <Card.Img src={url} style={{ width: '20%' }} />
